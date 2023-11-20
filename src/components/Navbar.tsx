517377bc191729fc86e5603dc4cf6a6448c54565
Samueelx <gsammygitau@gmail.com>
2023-11-20 17:27:50 +0300

    navbar sticks at the top.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { SlOptionsVertical } from "react-icons/sl";
 
 function Navbar() {
     return (
-        <header className="bg-slate-900 text-white">
+        <header className="sticky top-0 bg-slate-900 text-white">
             <nav className="flex justify-between p-6">
                 <div>
                     <h2 className="font-bold">TEAM STATS</h2>
