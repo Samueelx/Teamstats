13b3d19b4063cd802ab9da1e603c74a1669b43c5
Samueelx <gsammygitau@gmail.com>
2023-11-20 09:58:45 +0300

    initialize project and did the navbar.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,19 @@
+import { SlOptionsVertical } from "react-icons/sl";
+
+function Navbar() {
+    return (
+        <header className="bg-slate-900 text-white">
+            <nav className="flex justify-between p-6">
+                <div>
+                    <h2 className="font-bold">TEAM STATS</h2>
+                </div>
+                <div className="flex items-center">
+                    <p className="mx-4">Talanta</p>
+                    <SlOptionsVertical />
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar;
